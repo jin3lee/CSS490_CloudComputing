f50d5ca1260b83b681e118ccfaf36839ac54a9bf
Joseph Lee <jin3lee@gmail.com>
2015-11-06 00:37:31 -0800

    *connect consumer to storage and now creates blobs

---
+++ CP2_JosephLee/CpTwo/Consumer/Program.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.Azure;
+using System;
+
+namespace Consumer
+{
+    class Program
+    {
+
+        // Const Variables
+        private const String QUEUE_NAME = "queue1";
+
+        // consumes messages from the cloud's queue and prints it to console every 3 seconds
+        static void Main(string[] args)
+        {
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            QueueClient Client =
+              QueueClient.CreateFromConnectionString(connectionString, QUEUE_NAME);
+
+            // Configure the callback options.
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMilliseconds(3000);
+            
+            // Callback to handle received messages.
+            Client.OnMessage((message) =>
+            {
+                try
+                {
+                    string bodyMessage = "" + message.GetBody<string>();
+
+                    // Process message from queue
+                    Console.WriteLine();
+                    Console.WriteLine("Body: " + bodyMessage);
+                    Console.WriteLine("MessageID: " + message.MessageId);
+                    Console.WriteLine("Test Property: " +
+                    message.Properties["TestProperty"]);
+
+                    // write to blob
+                    Console.WriteLine("BEFORE retrieveConnectionString() " + bodyMessage);
+                    retrieveConnectionString("BODY: " + bodyMessage);
+                    Console.WriteLine("AFTER retrieveConnectionString()");
+
+                    // Remove message from queue.
+                    message.Complete();
+                }
+                catch (Exception)
+                {
+                    // Indicates a problem, unlock message in queue.
+                    message.Abandon();
+                }
+            }, options);
+
+
+            Console.ReadKey();
+        }
+        
+        public static void retrieveConnectionString(String text)
+        {
+            Console.WriteLine("CHECK 1");
+            //Parse the connection string for the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                Microsoft.Azure.CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            Console.WriteLine("CHECK 2");
+            //Create service client for credentialed access to the Blob service.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            Console.WriteLine("CHECK 3");
+            //Get a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("my-append-blobs");
+            Console.WriteLine("CHECK 4");
+            //Create the container if it does not already exist. 
+            container.CreateIfNotExists();
+            Console.WriteLine("CHECK 5");
+            //Get a reference to an append blob.
+            CloudAppendBlob appendBlob = container.GetAppendBlobReference("append-blob.log");
+            Console.WriteLine("CHECK 6");
+            //Create the append blob. Note that if the blob already exists, the CreateOrReplace() method will overwrite it.
+            //You can check whether the blob exists to avoid overwriting it by using CloudAppendBlob.Exists().
+            if (appendBlob == null)
+            {
+                System.Console.WriteLine("appendblob == null");
+            }
+            else
+            {
+                System.Console.WriteLine("appendblob != null");
+            }
+
+            appendBlob.CreateOrReplace();
+
+            int numBlocks = 10;
+
+            //Generate an array of random bytes.
+            Random rnd = new Random();
+            byte[] bytes = new byte[numBlocks];
+            rnd.NextBytes(bytes);
+
+            //Simulate a logging operation by writing text data and byte data to the end of the append blob.
+            for (int i = 0; i < numBlocks; i++)
+            {
+                appendBlob.AppendText(text + " " + i + "/n");
+            }
+
+            //Read the append blob to the console window.
+            Console.WriteLine(appendBlob.DownloadText());
+        }
+    }
+}
---
+++ CP2_JosephLee/CpTwo/Producer/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+using System;
+using Microsoft.Azure;
+
+namespace Producer
+{
+    class Program
+    {
+        // Const Variables
+        private const String QUEUE_NAME = "queue1";
+
+        // Other variables
+        private static int mProducerId = 0;
+
+        // sends specific messages to the cloud's queue containing producer's id and message id
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a numeric argument.");
+            }
+
+            if (args.Length != 0)
+            {
+                mProducerId = int.Parse(args[0]);
+                System.Console.WriteLine("Your producer number is: " + mProducerId);
+            }
+
+            initializeQueue();
+
+            Console.ReadKey();
+        }
+
+        // creates a queue if one is not created
+        static void initializeQueue()
+        {
+            // Create the queue if it does not exist already.
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+        
+            if (!namespaceManager.QueueExists(QUEUE_NAME))
+            {
+                namespaceManager.CreateQueue(QUEUE_NAME);
+                System.Console.WriteLine("Queue Created");
+                sendMessageToQueue();
+            }
+            else
+            {
+                System.Console.WriteLine("Queue Already Exists");
+                sendMessageToQueue();
+            }
+        }
+
+        // sends messages to the queue 
+        static void sendMessageToQueue()
+        {
+            string connectionString =
+            CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            QueueClient Client =
+                QueueClient.CreateFromConnectionString(connectionString, QUEUE_NAME);
+
+            for (int i = 0; ; i++)
+            {
+                // Create message, passing a string message for the body.
+                BrokeredMessage message = new BrokeredMessage("prod#" + mProducerId + " msg#" + i);
+
+                // Set some addtional custom app-specific properties.
+                message.Properties["TestProperty"] = "TestValue";
+                message.Properties["Message number"] = i;
+
+                // Send message to the queue.
+                Client.Send(message);
+
+                // Print message to console 
+                System.Console.WriteLine("\"prod#" + mProducerId + " msg#" + i + "\" was sent!");
+
+                // Wait 1 Second before continuing this loop
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    
+    }
+}
